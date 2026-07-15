f3839a16d12aae9ed50e37caa642017ac5b2796c
Merényi Miklós <89610165+merenyimiklos@users.noreply.github.com>
2026-07-15 22:56:55 +0200

    build(android): use Kotlin 2.3 compatible image and DI stack

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.02.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.3.21"))
 
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.12.3")
@@ -86,12 +87,12 @@ dependencies {
     implementation("com.google.maps.android:maps-compose-utils:6.12.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    implementation("io.coil-kt.coil3:coil-compose:3.5.0")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.5.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.4.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
     implementation("com.airbnb.android:lottie-compose:6.7.1")
 
-    implementation("com.google.dagger:hilt-android:2.57.1")
-    kapt("com.google.dagger:hilt-compiler:2.57.1")
+    implementation("com.google.dagger:hilt-android:2.60.1")
+    kapt("com.google.dagger:hilt-compiler:2.60.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
